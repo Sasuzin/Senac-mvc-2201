c3ff1a1134ba4608b87b2e14ec1bedffe3e82043
LUIS FELIPE GALVAO TORRADO <luis.fgtorrado@senacsp.edu.br>
2022-05-27 20:27:51 -0300

    JWTAuth criada

---
+++ primeiro_site/app/Http/Controllers/APIController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class APIController extends Controller
+{
+    public $loginAfterSignUp = true;
+
+    public function login(Request $request) 
+    {
+        $token = null;
+        $campos_json = json_decode( $request->getContent(), JSON_OBJECT_AS_ARRAY);
+        $credenciais = ['email' => $campos_json['email'],
+                        'password' => $campos_json['password']];
+
+        try{
+
+            if(!$token = JWTAuth::attempt($credenciais)){
+                return response()->json(['sucess' => false,
+                                        'message' => 'Credenciais Invalidas'], 401);
+            }
+
+        }catch(JWWTException $e){
+            return response()->json(['error' =>'Token não criado'],500);
+        }
+        return response()->json(['success' => true,
+                                    'token' => $token],200);
+    }
+}
